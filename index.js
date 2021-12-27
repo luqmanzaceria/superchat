bedf44a4395f2742c5c3bdde28b35a9e2d61cba8
Luqman Zaceria <luqmanzaceria@gmail.com>
2021-12-26 21:52:42 -0800

    Fixed server reference

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-const port = process.env.PORT || 3000;
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
